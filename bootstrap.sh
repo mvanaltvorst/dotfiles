5f52d78ace664a51197e551c1819e446c2e369e7
Maurits van Altvorst <mvanaltvorst@icloud.com>
2018-12-09 15:53:28 +0100

    Add .tmux.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
-echo "Copying zshrc..."
+echo "Copying .zshrc..."
 cp ./.zshrc ~/.zshrc
-echo "Copying vimrc..."
+echo "Copying .vimrc..."
 cp ./.vimrc ~/.vimrc
+echo "Copying .tmux.conf..."
+cp ./.tmux.conf ~/.tmux.conf
