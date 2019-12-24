0d4a3d8de949376434d1189d486257d6fb421221
Maurits van Altvorst <mvanaltvorst@icloud.com>
2019-12-24 17:32:41 +0100

    Update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+export INTERFACE="$(ip link show | grep \<BROADCAST | awk -F':' '{print $2}' | xargs)"
+echo $INTERFACE
+
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
+polybar --reload top &
 
 echo "Polybar launched..."
