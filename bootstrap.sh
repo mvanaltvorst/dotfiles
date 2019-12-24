413907cd7905fa51a517ea9a230bc1f4387a8dd1
Maurits van Altvorst <mvanaltvorst@icloud.com>
2019-12-24 14:39:20 +0100

    Update dotfiles

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-echo "Copying .zshrc..."
-cp ./.zshrc ~/.zshrc
-echo "Copying .vimrc..."
-cp ./.vimrc ~/.vimrc
-echo "Copying .tmux.conf..."
-cp ./.tmux.conf ~/.tmux.conf
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: install.sh
+#
+#         USAGE: ./install.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Nima Saed (), nima.saed@me.com
+#  ORGANIZATION:
+#       CREATED: 06/10/2019 09:34:22 AM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
+ln -sfn ${PWD}/.scripts/ ${HOME}
+ln -sfn ${PWD}/.wallpaper ${HOME}
+ln -sfn ${PWD}/.Xresources ${HOME}
+ln -sfn ${PWD}/.zshrc ${HOME}
+ln -sfn ${PWD}/.tmux.conf  ${HOME}
+ln -sfn ${PWD}/.vimrc ${HOME}
+ln -sfn ${PWD}/i3/ ${HOME}/.config/
+ln -sfn ${PWD}/.colors/ ${HOME}
+ln -sfn ${PWD}/.dircolors/ ${HOME}
