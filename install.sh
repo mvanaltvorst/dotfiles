727e6bb1d51763de42372d7015805189ed523c57
Maurits van Altvorst <mvanaltvorst@icloud.com>
2021-05-15 18:25:07 +0200

    Start dotfiles fresh on macOS for ARM

--- install.sh
+++ install.sh
@@ -1,27 +1,4 @@
 #!/bin/bash -
-#===============================================================================
-#
-#          FILE: install.sh
-#
-#         USAGE: ./install.sh
-#
-#   DESCRIPTION:
-#
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: Maurits van Altvorst, nima.saed@me.com
-#  ORGANIZATION:
-#       CREATED: 06/10/2019 09:34:22 AM
-#      REVISION:  ---
-#===============================================================================
-
 set -o nounset                                  # Treat unset variables as an error
 
-ln -sfn ${PWD}/.zshrc ${HOME}
-ln -sfn ${PWD}/.tmux.conf  ${HOME}
-ln -sfn ${PWD}/nvim/ ${HOME}/.config/
-ln -sfn ${PWD}/alacritty/ ${HOME}/.config/
-ln -sfn ${PWD}/.doom.d/ ${HOME}/
-
+ln -sfn ${PWD}/emacs ${HOME}/.config/
